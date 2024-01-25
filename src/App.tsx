ed64dbe8075d9728502453c50235397029bab8f4
akbiv <akbivash@gmail.com>
2024-01-25 13:17:28 +0545

    Format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
 import { CarouselWrapper } from "./components/CarouselWrapper";
 import { Item } from "./components/Item";
-import { CarouselContext } from "./context/CarouselContext";
 import { ICarouselWrapperConfig } from ".";
 
 const App = () => {
   const config: ICarouselWrapperConfig = {
     height: 200,
     responsive: {
-      sm: { breakPoint: 500, itemPerScreen: 2 },
+      sm: {
+        breakPoint: 500,
+        itemPerScreen: 2,
+      },
     },
+    arrow: false,
   };
 
   return (
     <div className="grid   " data-test="app">
-      <CarouselContext>
-        <CarouselWrapper config={config}>
-          <Item>
-            <div className="grid place-items-center h-full">1</div>
-          </Item>
-          <Item>
-            {" "}
-            <div className="grid place-items-center h-full">4</div>
-          </Item>
-        </CarouselWrapper>
-      </CarouselContext>
+      <CarouselWrapper config={config}>
+        <Item>
+          <div>1</div>
+        </Item>
+        <Item>
+          {" "}
+          <div>2</div>
+        </Item>
+        <Item>
+          <div>1</div>
+        </Item>
+        <Item>
+          {" "}
+          <div>2</div>
+        </Item>
+        <Item>
+          <div>1</div>
+        </Item>
+        <Item>
+          {" "}
+          <div>2</div>
+        </Item>
+      </CarouselWrapper>
     </div>
   );
 };
